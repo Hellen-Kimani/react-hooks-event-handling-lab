9a301f20f292447e7f741184882ca379ff47ef59
hellen kimani <astridlee80@gmail.com>
2024-05-12 19:18:46 +1000

    First commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,5 @@
-// Code EyesOnMe Component Here
+export default function EyesOnMe (){
+    return (
+        <button onFocus={(e)=>console.log('Good!')} onBlur={(e)=>console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    )
+}
